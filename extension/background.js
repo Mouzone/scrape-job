f3136905056833e1f8d2d0219ab5cac52869e0bf
Mouzone <sunnyliu010@gmail.com>
2025-01-06 20:09:33 -0500

    Refactor icons to images to work with manifest and properly select heading and jobtitle for glassdoor

--- extension/background.js
+++ extension/background.js
@@ -1,10 +1,27 @@
-const glassdoor = "https://www.workatastartup.com/jobs/";
-const yCombinator = "https://www.glassdoor.com/Job/index.htm";
+const glassdoor = "https://www.glassdoor.com/Job/index.htm";
+const yCombinator = "https://www.workatastartup.com/jobs/";
 
-chrome.actions.onClicked.addListener(async (tab) => {
-    if (tab.startsWith(glassdoor)) {
-        tab.scripting.executeScript("glassdoor.js")
-    } else {
-        tab.scripting.executeScript("y-comb.js")
+chrome.action.onClicked.addListener(async (tab) => {
+    if (tab.url.startsWith(glassdoor)) {
+        chrome.scripting.executeScript(
+            {
+                target: {tabId: tab.id},
+                files: ["glassdoor.js"]
+            }
+        );
+    } else if (tab.url.startsWith(yCombinator)) {
+        chrome.scripting.executeScript(
+            {
+                target: {tabId: tab.id},
+                files: ["yCombinator.js"]
+            }
+        );
     }
 })
+
+// todo: write the scripts and the fetch query inside the scripts once injected
+// create db in supabase
+// write the backend in go
+// host backend on railway(?)
+// figure out how to protect the api endpoint
+
---
+++ extension/glassdoor.js
@@ -0,0 +1,4 @@
+const companyName = document.querySelector("h4.heading_Heading__BqX5J.heading_Subhead__Ip1aW")
+const jobTitle = document.querySelector("h1.heading_Heading__BqX5J.heading_Level1__soLZs")
+
+console.log(companyName.textContent, jobTitle.textContent)
