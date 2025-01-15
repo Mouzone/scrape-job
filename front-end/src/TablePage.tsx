dac85afad6cbc60a758d650e5a8bd91ec0fab280
Mouzone <sunnyliu010@gmail.com>
2025-01-14 20:24:42 -0500

    Minor styling to the search bar

--- front-end/src/TablePage.tsx
+++ front-end/src/TablePage.tsx
@@ -58,7 +58,7 @@ function Search({type, searchTerm, setSearchTerm, searchValue, setSearchValue}) 
             <select
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
-                className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                className=" px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
             >
                 {columns[type].map((column) => (
                     <option key={column} value={column}>
