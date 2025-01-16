c448ce6ca383c68fb21a2d9b509041d91e78b0a7
Mouzone <sunnyliu010@gmail.com>
2025-01-16 12:20:08 -0500

    Refactor all of Table into separate components

--- front-end/src/Table.tsx
+++ front-end/src/Table.tsx
@@ -42,50 +42,8 @@ export default function Table({ type, error, isLoading, data }) {
             <DeleteConfirmation deleteEntry={deleteEntry} toDelete={toDelete} setToDelete={setToDelete}/>
 			<div className="overflow-x-auto rounded-lg border border-gray-200">
 				<table className="w-full border-collapse">
-					<thead>
-						<tr className="bg-gray-50">
-							<th className="p-4 text-left text-gray-600 font-medium border-b">#</th>
-							{columns[type].map(header => (
-								<th key={header} className="p-4 text-left text-gray-600 font-medium border-b">
-									{header.charAt(0).toUpperCase() + header.substring(1)}
-								</th>
-							))}
-                            <th className="p-4 text-left text-gray-600 font-medium border-b"> Delete </th>
-						</tr>
-					</thead>
-					<tbody>
-						{paginated.map((entry, index) => (
-							<tr 
-								key={pageIndex + index + 1}
-								className="hover:bg-gray-50 transition-colors"
-							>
-								<td className="p-4 border-b text-gray-500">
-									{pageIndex + index + 1}
-								</td>
-								{columns[type].map((column) => (
-									<td key={column} className="p-4 border-b">
-										{entry[column]}
-									</td>
-								))}
-                                <td className="flex items-center justify-center p-4 border-b">
-                                    <button 
-                                        className="flex items-center justify-center hover:bg-gray-100 rounded p-2 transition-colors"
-                                        type="button"
-                                        onClick={() => setToDelete(entry["id"])}
-                                    >
-                                        <svg 
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 24 24"
-                                            className="fill-red-500 w-5"
-                                            >
-                                            <title>delete</title>
-                                            <path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
-                                        </svg>
-                                    </button>
-                                </td>
-							</tr>
-						))}
-					</tbody>
+					<TableHead type={type}/>
+                    <TableBody type={type} pageIndex={pageIndex} data={paginated} setToDelete={toDelete}/>
 				</table>
                 <Nav pageIndex={pageIndex} setPageIndex={setPageIndex} filtered={data}/>
 			</div>
@@ -119,12 +77,71 @@ function DeleteConfirmation({toDelete, setToDelete, deleteEntry}) {
     )
 }
 
-function TableHead() {
-
+function TableHead({type}) {
+    return (
+        <thead>
+            <tr className="bg-gray-50">
+                <th className="p-4 text-left text-gray-600 font-medium border-b">#</th>
+                {columns[type].map(header => (
+                    <th key={header} className="p-4 text-left text-gray-600 font-medium border-b">
+                        {header.charAt(0).toUpperCase() + header.substring(1)}
+                    </th>
+                ))}
+                <th className="p-4 text-left text-gray-600 font-medium border-b"> Delete </th>
+            </tr>
+        </thead>
+    )
 }
 
-function TableBody() {
+function TableBody({type, data, pageIndex, setToDelete}) {
+    return (
+        <tbody>
+            {
+                data.map((entry, index) => (
+                    <tr 
+                        key={pageIndex + index + 1}
+                        className="hover:bg-gray-50 transition-colors"
+                    >
+                        <td className="p-4 border-b text-gray-500">
+                            {pageIndex + index + 1}
+                        </td>
+                        {columns[type].map((column) => (
+                            <ModifiableCell key={column} value={entry[column]}/>
+                        ))}
+                        <DeleteButton setToDelete={setToDelete} id={entry["id"]}/>
+                    </tr>
+                ))
+            }
+        </tbody>
+    )
+}
 
+function ModifiableCell({key, value}) {
+    return (
+        <td key={key} className="p-4 border-b">
+            {value}
+        </td>
+    )
+}
+function DeleteButton({setToDelete, id}) {
+    return (
+        <td className="flex items-center justify-center p-4 border-b">
+            <button 
+                className="flex items-center justify-center hover:bg-gray-100 rounded p-2 transition-colors"
+                type="button"
+                onClick={() => setToDelete(id)}
+            >
+                <svg 
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    className="fill-red-500 w-5"
+                    >
+                    <title>delete</title>
+                    <path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
+                </svg>
+            </button>
+        </td>
+    )
 }
 
 function Nav({pageIndex, setPageIndex, filtered}) {
