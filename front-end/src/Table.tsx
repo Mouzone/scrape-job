1635268db31644e940b0686a9074e1e1abb7f408
Mouzone <sunnyliu010@gmail.com>
2025-01-16 20:59:25 -0500

    Properly modify order of entries based on application time

--- front-end/src/Table.tsx
+++ front-end/src/Table.tsx
@@ -9,8 +9,8 @@ const columns = {
 export default function Table({ type, error, isLoading, data }) {
 	const [pageIndex, setPageIndex] = useState(0);    
     const [toDelete, setToDelete] = useState(null);
-    const [increasing, setIncreasing] = useState(false);
-    console.log(type, error, isLoading, increasing)
+    const [increasing, setIncreasing] = useState(true);
+
     useEffect(() => {
         setPageIndex(0)
     }, [type])
@@ -104,9 +104,8 @@ function TableHead({type, setIncreasing, increasing}) {
                             {
                                 header === "applied" && (
                                     increasing
-                                    ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>menu-up</title><path d="M7,15L12,10L17,15H7Z" /></svg>
-                                    : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>menu-down</title><path d="M7,10L12,15L17,10H7Z" /></svg>
-                                    
+                                    ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>menu-down</title><path d="M7,10L12,15L17,10H7Z" /></svg>
+                                    : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>menu-up</title><path d="M7,15L12,10L17,15H7Z" /></svg>
                                 )
                             }
                         </th>
