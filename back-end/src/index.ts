d2f21f805abcf7c8a27bd931686e123207f66bd5
Mouzone <sunnyliu010@gmail.com>
2025-01-20 19:41:10 -0500

    Reformat spaces in index.ts and order job entries by "id" since it is fastest way to preserve order of jobs applied in

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -6,61 +6,62 @@ import { JobSchema, AccountSchema } from "../types";
 import formatDateTime from "./utility/formatDateTime";
 
 const app = new Elysia()
-    .use(cors())
-    .post("/jobs", async ({body}) => {
-        console.log(body);
-        await addJob(body)
-    }, {
-       body: JobSchema
-    })
-    .post("/accounts", async ({body}) => {
-        console.log(body)
-        await addAccount(body)
-    }, {
-        body: AccountSchema
-    })
-    .get("/jobs", async() => {
-       const results = await getJobs()
-    //  modifies the objects inside results, so we don't have to set the output to another array
-       results.map(result => result["applied"] = formatDateTime(result["applied"]))
-       return results
-    })
-    .get("/accounts", async () => {
-        return await getAccounts()
-    })
-    .delete("/jobs", async({body}) => {
-        await deleteJob(body["id"])
-    }, {
-        body: t.Object({
-            id: t.Integer()
-        })
-    })
-    .delete("/accounts", async({body}) => {
-        await deleteAccount(body["id"])
-    }, {
-        body: t.Object({
-            id: t.Integer()
-        })
-    })
-    .put("/jobs", async({body}) => {
-        await updateJob(body["id"], body["column"], body["newValue"])
-    }, {
-        body: t.Object({
-            id: t.Integer(),
-            column: t.String(),
-            newValue: t.String()
-        })
-    })
-    .put("/accounts", async({body}) => {
-        await updateAccount(body["id"], body["column"], body["newValue"])
-    }, {
-        body: t.Object({
-            id: t.Integer(),
-            column: t.String(),
-            newValue: t.String()
-        })
-    })
-    .listen(3000);
+	.use(cors())
+	.post("/jobs", async ({body}) => {
+		console.log(body);
+		await addJob(body)
+	}, {
+	   body: JobSchema
+	})
+	.post("/accounts", async ({body}) => {
+		console.log(body)
+		await addAccount(body)
+	}, {
+		body: AccountSchema
+	})
+	.get("/jobs", async() => {
+	   const results = await getJobs()
+	//  modifies the objects inside results, so we don't have to set the output to another array
+		results.forEach(result => result["applied"] = formatDateTime(result["applied"]))
+		results.sort((a, b) => a["id"] - b["id"])
+		return results
+	})
+	.get("/accounts", async () => {
+		return await getAccounts()
+	})
+	.delete("/jobs", async({body}) => {
+		await deleteJob(body["id"])
+	}, {
+		body: t.Object({
+			id: t.Integer()
+		})
+	})
+	.delete("/accounts", async({body}) => {
+		await deleteAccount(body["id"])
+	}, {
+		body: t.Object({
+			id: t.Integer()
+		})
+	})
+	.put("/jobs", async({body}) => {
+		await updateJob(body["id"], body["column"], body["newValue"])
+	}, {
+		body: t.Object({
+			id: t.Integer(),
+			column: t.String(),
+			newValue: t.String()
+		})
+	})
+	.put("/accounts", async({body}) => {
+		await updateAccount(body["id"], body["column"], body["newValue"])
+	}, {
+		body: t.Object({
+			id: t.Integer(),
+			column: t.String(),
+			newValue: t.String()
+		})
+	})
+	.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
