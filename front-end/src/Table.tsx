94e0fa91063d930ba511a632cf678c8cce075231
Mouzone <sunnyliu010@gmail.com>
2025-01-21 19:52:01 -0500

    Format applied date on the front end

--- front-end/src/Table.tsx
+++ front-end/src/Table.tsx
@@ -151,6 +151,16 @@ function ModifiableCell({type, id, column, value}: {type: Type, id: number, colu
     const [edit, setEdit] = useState(false)
     const [newValue, setNewValue] = useState(value)
 
+    if (column === "applied") {
+        const date = new Date(value)
+        const formattedDate = date.toLocaleDateString("en-US", {
+            year: "2-digit",
+            month: "2-digit",
+            day: "2-digit"
+          })
+        value = formattedDate
+    }
+
     const sendEdit = () => {
         setEdit(false)
         fetch("http://localhost:3000/" + type, {
