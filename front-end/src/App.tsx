fd7c679de8e84acce67ec27f853bc0285f48a3a5
Mouzone <sunnyliu010@gmail.com>
2025-03-21 23:23:19 -0400

    Initalize nextjs refactor

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -4,26 +4,36 @@ import TablePage from "./TablePage";
 import TabButton from "./TabButton";
 
 function App() {
-	const [page, setPage] = useState<"table" | "inputs">("table");
+    const [page, setPage] = useState<"table" | "inputs">("table");
 
-	return (
-		<div className="min-h-screen bg-gray-100 p-4 md:p-8">
-			<div className="mx-auto max-w-6xl bg-white rounded-xl shadow-md">
-				<div className="border-b border-gray-200">
-					<div className="p-4 md:p-6">
-						<nav className="flex gap-2 md:gap-4">
-                            <TabButton onClick={() => setPage("table")} text="View Data" value="table" data={page}/>
-                            <TabButton onClick={() => setPage("inputs")} text="Create New" value="inputs" data={page}/>
-						</nav>
-					</div>
-				</div>
+    return (
+        <div className="min-h-screen bg-gray-100 p-4 md:p-8">
+            <div className="mx-auto max-w-6xl bg-white rounded-xl shadow-md">
+                <div className="border-b border-gray-200">
+                    <div className="p-4 md:p-6">
+                        <nav className="flex gap-2 md:gap-4">
+                            <TabButton
+                                onClick={() => setPage("table")}
+                                text="View Data"
+                                value="table"
+                                data={page}
+                            />
+                            <TabButton
+                                onClick={() => setPage("inputs")}
+                                text="Create New"
+                                value="inputs"
+                                data={page}
+                            />
+                        </nav>
+                    </div>
+                </div>
 
-				<div className="p-4 md:p-6">
-					{page === "table" ? <TablePage /> : <Forms />}
-				</div>
-			</div>
-		</div>
-	);
+                <div className="p-4 md:p-6">
+                    {page === "table" ? <TablePage /> : <Forms />}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
